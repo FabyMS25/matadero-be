db92995064e66200b76ae2b70d8e8e7634276106
Gamq Developers <devgamq@gmail.com>
2024-11-04 18:16:34 -0400

    repos

---
+++ src/main/java/gamq/recaudaciones/matadero/Controller/ReportesController.java
@@ -0,0 +1,62 @@
+package gamq.recaudaciones.matadero.Controller;
+
+
+import gamq.recaudaciones.matadero.utils.GeneradorReporte;
+import net.sf.jasperreports.engine.JRParameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/reporte")
+public class ReportesController {
+    @Autowired
+    private GeneradorReporte generadorReporte;
+
+    @RequestMapping(value="/ordenes",method= RequestMethod.GET)
+    @ResponseBody
+    public void reporteListaReserva (
+            @RequestParam(name = "fecha_ini",required = false) @DateTimeFormat(pattern="dd/MM/yyyy") Date fecha_ini,
+            @RequestParam(name = "fecha_fin",required = false) @DateTimeFormat(pattern="dd/MM/yyyy") Date fecha_fin,
+            @RequestHeader Map<String, String> headers,
+            HttpServletResponse response
+    ) {
+        try {
+            //usuario
+            //String usuario = headers.getOrDefault("usuario", "Default");
+            //ingreso almacen
+            HashMap<String, Object> parametros = new HashMap<String,Object>();
+            parametros.put("fecha_ini",fecha_ini);
+            parametros.put("fecha_fin",fecha_fin);
+
+            //parametros.put("logo64","iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk");
+
+            parametros.put(JRParameter.REPORT_LOCALE, Locale.ENGLISH);
+            try {
+                generadorReporte.generarSqlReportePdf(
+                        "ordenes",
+                        "classpath:reportes/ordenes.jrxml",
+                        parametros,
+                        response
+                );
+            } catch (SQLException ex) {
+                response.setStatus(500);
+                throw new RuntimeException(ex);
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+
+}
