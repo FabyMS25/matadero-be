92e525611b4c25ae66b4e83521b4b3ec303ed24b
Gamq Developers <devgamq@gmail.com>
2024-11-04 16:30:51 -0400

    Controller

--- src/main/java/gamq/recaudaciones/matadero/Controller/CategoriaController.java
+++ src/main/java/gamq/recaudaciones/matadero/Controller/CategoriaController.java
@@ -1,11 +1,56 @@
 package gamq.recaudaciones.matadero.Controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import gamq.recaudaciones.matadero.Dto.CategoriaDto;
+import gamq.recaudaciones.matadero.Dto.response.Response;
+import gamq.recaudaciones.matadero.Service.CategoriaService;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categoria")
 public class CategoriaController {
+   @Autowired
+    CategoriaService categoriaService;
+    @GetMapping("/{uuid}")
+    public Response buscarCategoriaPorUuid(@Parameter(description = "Uuid para busqueda de actividad economica")
+                                               @PathVariable("uuid") String uuidConstruccion) {
+        try{
+            CategoriaDto contri = categoriaService.findByUuid(uuidConstruccion);
+            return Response.ok().setPayload(contri);
+        } catch(Exception ex) {
+            return Response.notFound().setPayload(ex.getMessage());
+        }
+    }
+    @GetMapping()
+    public Response obtenerCategorias() {
+        try{
+            List<CategoriaDto> contris =  categoriaService.obtenerCategorias();
+            return Response.ok().setPayload(contris);
+        } catch(Exception ex) {
+            return Response.notContent().setPayload(ex.getMessage());
+        }
+    }
+    @PostMapping()
+    public Response crearCategoria(@Parameter(description = "Objeto json para crear una Construccion")
+                                       @RequestBody CategoriaDto CategoriaDto) {
+        try{
+            return Response.ok().setPayload(categoriaService.crearCategoria(CategoriaDto));
+        } catch (Exception ex) {
+            return  Response.unprocessableEntity().setPayload(ex.getMessage());
+        }
+    }
+    @PutMapping()
+    public Response actualizarCategoria(@Parameter(description = "Objeto json para actualizar Construccion")
+                                            @RequestBody CategoriaDto CategoriaDto) {
+        try{
+            return Response.ok().setPayload(categoriaService.actualizarCategoria(CategoriaDto));
+        } catch (Exception ex) {
+            return  Response.unprocessableEntity().setPayload(ex.getMessage());
+        }
+    }
 
 }
