251921d31a5fb1dcdf3ffd0de8cd7c0fdce9dc5c
Gamq Developers <devgamq@gmail.com>
2025-12-29 09:15:22 -0400

    swagger

--- src/main/java/gamq/recaudaciones/matadero/Controller/SolicitudController.java
+++ src/main/java/gamq/recaudaciones/matadero/Controller/SolicitudController.java
@@ -3,7 +3,9 @@ package gamq.recaudaciones.matadero.Controller;
 import gamq.recaudaciones.matadero.Dto.SolicitudDto;
 import gamq.recaudaciones.matadero.Dto.response.Response;
 import gamq.recaudaciones.matadero.Service.SolicitudService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +16,13 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/solicitud")
+@Tag(name = "Solicitudes", description = "Servicio de Administracion de Solicitudes")
 public class SolicitudController {
 
     @Autowired
     SolicitudService SolicitudService;
 
+    @Operation(summary = "Buscar una Solicitud por uuid")
     @GetMapping("/{uuid}")
     public Response buscarSolicitudPorUuid(@Parameter(description = "Uuid para busqueda de actividad economica")
                                           @PathVariable("uuid") String uuidConstruccion) {
@@ -29,6 +33,7 @@ public class SolicitudController {
             return Response.notFound().setPayload(ex.getMessage());
         }
     }
+    @Operation(summary = "Buscar una Solicitud por id")
     @GetMapping("/por/{id}")
     public Response buscarSolicitudPorId(@Parameter(description = "Uuid para busqueda de actividad economica")
                                            @PathVariable("id") Long id) {
@@ -39,6 +44,7 @@ public class SolicitudController {
             return Response.notFound().setPayload(ex.getMessage());
         }
     }
+    @Operation(summary = "Listar todas las Solicitudes")
     @GetMapping()
     public Response obtenerSolicituds() {
         try{
@@ -48,6 +54,7 @@ public class SolicitudController {
             return Response.notContent().setPayload(ex.getMessage());
         }
     }
+    @Operation(summary = "Listar todas las Solicitudes entre un intervalo de fechas")
     @GetMapping("/fechas")
     public Response buscarSolictudesPorFechaSalida(
             @Parameter(name = "fechaIni",description = "Fecha Salida inicio")
@@ -63,6 +70,7 @@ public class SolicitudController {
                 }
 
     }
+    @Operation(summary = "Crea una Solicitud")
     @PostMapping()
     public Response crearSolicitud(@Parameter(description = "Objeto json para crear una Construccion")
                                   @RequestBody SolicitudDto SolicitudDto) {
@@ -72,6 +80,7 @@ public class SolicitudController {
             return  Response.unprocessableEntity().setPayload(ex.getMessage());
         }
     }
+    @Operation(summary = "Actualiza una Solicitud")
     @PutMapping()
     public Response actualizarSolicitud(@Parameter(description = "Objeto json para actualizar Construccion")
                                        @RequestBody SolicitudDto SolicitudDto) {
@@ -81,17 +90,20 @@ public class SolicitudController {
             return  Response.unprocessableEntity().setPayload(ex.getMessage());
         }
     }
+    @Operation(summary = "Cambia el estado de la solicitud de uuid")
     @PutMapping("/{uuid}/estado")
     public Response cambiar_Estado(@Parameter(description = "Uuid para cambiar estado  Solicitud")
                                        @PathVariable("uuid") String uuid) {
         return Response.ok().setPayload(SolicitudService.Cambiar_estado(uuid));
 
     }
+    @Operation(summary = "Elimina una Solicitude por uuid")
     @DeleteMapping("/{uuid}")
     public Response delete(@Parameter(description = "Uuid para eliminar Solicitud")
                                @PathVariable("uuid") String uuid) {
         return Response.ok().setPayload(SolicitudService.delete(uuid));
     }
+    @Operation(summary = "Borrado Logico de Una Solicitud")
     @PutMapping("/soft-delete")
     public Response softDelete(
             @RequestParam String uuid,
